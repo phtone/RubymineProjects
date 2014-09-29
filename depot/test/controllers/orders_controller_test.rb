30f30930195e757cf97952c05746c1c28cf24c69
phtone <phtone@gmail.com>
2014-09-29 23:59:10 +0400

    enter information about ordering 182

--- depot/test/controllers/orders_controller_test.rb
+++ depot/test/controllers/orders_controller_test.rb
@@ -1,4 +1,18 @@
-require 'test_helper'
+test "requires item in cart" do
+  get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+end
+
+test "should get new" do
+  item = LineItem.new
+  item.build_cart
+  item.product = products(:ruby)
+  item.save!
+  session[:cart_id] = item.cart.id
+  get :new
+  assert_response :success
+end
 
 class OrdersControllerTest < ActionController::TestCase
   setup do
